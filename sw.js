76c800032310b8130d6b55df476c1b086f51a9fc
ricardoV94 <ricardo.vieira1994@gmail.com>
2026-04-02 23:02:14 +0200

    Cooldown on todo list delete

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'onelist-2026-04-02 12:44';
+const CACHE_NAME = 'onelist-2026-04-02 23:00';
 const SHELL_FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
