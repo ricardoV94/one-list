b903db073ef786e74bfa449d01f709bd111d4406
ricardoV94 <ricardo.vieira1994@gmail.com>
2026-05-31 14:09:53 +0200

    Message too long status

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'onelist-2026-05-30 15:20';
+const CACHE_NAME = 'onelist-2026-05-31 13:56';
 const SHELL_FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 const CDN_FILES = [
   'https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js',
