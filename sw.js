ae5b51248e66f823da48525c64810cd4720cbe9e
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2026-06-01 16:16:47 +0200

    Fix empty space in todo sentences

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'onelist-2026-06-01 15:20';
+const CACHE_NAME = 'onelist-2026-06-01 16:14';
 const SHELL_FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 const CDN_FILES = [
   'https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js',
